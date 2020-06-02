ed14c80edbd1d98e4dcede4cb23fd453c22ccd5c
Song <quanpengsong@gmail.com>
2020-06-02 15:11:10 +0800

    :construction: save work

--- app/src/main/java/com/song/chat/MainActivity.java
+++ app/src/main/java/com/song/chat/MainActivity.java
@@ -1,11 +1,8 @@
 package com.song.chat;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -26,18 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
@@ -49,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
--- app/src/main/java/com/song/chat/ui/home/HomeFragment.java
+++ app/src/main/java/com/song/chat/ui/home/HomeFragment.java
@@ -1,35 +1,51 @@
 package com.song.chat.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import com.song.chat.R;
 
 public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
+    private EditText messageInput;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        messageInput = root.findViewById(R.id.message_input);
+        Button sendBtn = root.findViewById(R.id.send_btn);
+        sendBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.i("user", "click send btn");
+                sendBtnClicked(true);
+            }
+        });
+        sendBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.i("user", "long click send btn");
+                sendBtnClicked(false);
+                return false;
+            }
+        });
         return root;
+    }
+
+    private void sendBtnClicked(boolean isTextMessage){
+        if(isTextMessage){
+            String messageContent = messageInput.getText().toString().trim();
+            if(messageContent.equals("")) return;
+            Log.i("user send message", messageContent);
+            // TODO: use socket to send message here.
+            messageInput.setText("");
+        }
+        // TODO: select image from phone, upload & then send image url.
     }
 }
